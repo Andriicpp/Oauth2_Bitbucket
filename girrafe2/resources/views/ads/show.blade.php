c4880e255029c20899b5299bdbe484458516f4f3
andrii <git@github.com:Andriicpp/test.git>
2021-11-06 19:03:56 +0200

    tetst

--- girrafe2/resources/views/ads/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('content')
-        <h2>{{ $ad->title }}</h2>
-        <p>{{ $ad->user->username }}</p>
-        <p>{{ $ad->created_at->diffForHumans() }}</p>
-        <p>{{ $ad->description }}</p>
-@endsection
